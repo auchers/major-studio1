c3c71612ed3e6924da43dd2a372bab3ebbfe0bca
Aucher Serr <serra774@newschool.edu>
2017-09-24 13:37:53 +0000

    add axes for 2 countries

--- projects/project1/script.js
+++ projects/project1/script.js
@@ -1,20 +1,9 @@
 // window variables
 var margin = {top: 20, right: 50, bottom: 50, left: 20};
-var numCountries = 5;
+var numCountries = 3;
 
-var width = window.innerWidth - margin.left - margin.right,
+var width = (window.innerWidth - margin.left - margin.right),
     height = (window.innerHeight - margin.top - margin.bottom) / numCountries;
-
-// GLOBALS -------------
-// Parse the date / time
-// var	parseDate = d3.timeFormat("%Y");
-
-
-
-// Define the line
-// var	valueline = d3.svg.line()
-// 	.x(function(d) { return x(d.Year); })
-// 	.y(function(d) { return y(d.Gini); });
 
 // GRAPHING ---------
 
@@ -32,29 +21,21 @@ d3.json('kyrgyzstan_subset.json', function(error, data){
     graph(data, 'kyrgyzstan');
 })
 
-
+// Graph Function
 function graph(data, countryID){
-    // data.Year = parseDate(data.Year);
     var years = [];
     var ginis = [];
-    var radius = 3;
+    var radius = 5;
+    var scaleBuffer = 1;
     
     data.forEach(function(d){
         years.push(d.Year);
         ginis.push(d.Gini);
     });
     
-    // create svg and g    
-    var svg = d3.select('#'+countryID)
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
     // Set the ranges
     var	x = d3.scaleLinear()
-        .domain([d3.min(years), d3.max(years)])
+        .domain([d3.min(years) - 1, d3.max(years)])
         .range([0, width]);
         
     var	y = d3.scaleLinear()
@@ -67,34 +48,50 @@ function graph(data, countryID){
     // Define the axes
     var	xAxis = d3.axisBottom(x).ticks(5);
     var	yAxis = d3.axisLeft(y).ticks(5);
+    
+    // create svg and g    
+    var svg = d3.select('#'+countryID)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .call(yAxis)
+        .attr('class', 'yAxis');
+    
+    svg.append('g')
+        .attr("transform", 'translate(0,' + height + ')')
+        .attr('class', 'xAxis')
+        .call(xAxis);
     	
     svg.append('g')
-        .attr('class', 'group');
+        .attr('class', 'group')
+        
         
     var group = svg.select('.group')
         .selectAll('g')
         .data(data)
         .enter()
         .append('g')
-        // .attr('class', function(d){ return d.Year})
         .attr('class', function(d){ return d.Country + ' ' + d.Year + ' '
             + d.YearsTillRev + ' ' + d.Quality + ' ' + d.Gini})
         .attr('x', function(d,i){return x(d.Year);})
-        // .attr('transform', function(d, i){
-        //     return 'translate(' + x(d.YearsTillRev) + ', ' + 0 + ')';
-        // });
         
-        group.append('circle')
-            .attr('class', 'point')
-            .attr('cy', function(d, i){ return y(d.Gini)})
-            .attr('cx', function(d, i){ return x(d.Year)})
-            .attr('r', radius);
-        
-        group.append('text')
-            .text(function(d){ return d.Year})
-            .style('text-anchor', 'middle')
-            .attr('x', function(d, i){ return x(d.Year)})
-            .attr('y', height);
+    group.append('circle')
+        .attr('class', 'point')
+        .attr('class', function (d){
+                    if (d.Year == d.Revyear)
+                        return 'Revyear';
+                })
+        .attr('cy', function(d, i){ return y(d.Gini)})
+        .attr('cx', function(d, i){ return x(d.Year)})
+        .attr('r', radius);
+    
+    group.append('text')
+        .text(function(d){ return d.Year})
+        .style('text-anchor', 'middle')
+        .attr('x', function(d, i){ return x(d.Year)})
+        .attr('y', height);
         
 
 }
