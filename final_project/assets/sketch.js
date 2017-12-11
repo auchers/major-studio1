d6390f62ce90c872dc670cc8914e217772697a73
serrau <aucher.serr@gmail.com>
2017-12-10 19:51:58 -0500

    new data, crops out of whole, collapse sorting and scaling to 1 action

--- final_project/assets/sketch.js
+++ final_project/assets/sketch.js
@@ -1,69 +1,33 @@
-var metricMapping = {
+var scaleMapping ={
     'Worker Productivity': {
         "dataName": "AgriValuePerWorker",
+        "scaleType": "Linear",
         "fullName": "Agriculture value added per worker (constant 2010 US$)",
         "unit_long": "(constant 2010 US$)",
         "unit": "$",
         "description": "Agriculture value added per worker is a measure of agricultural productivity. Value added in agriculture measures the output of the agricultural sector (ISIC divisions 1-5) less the value of intermediate inputs. Agriculture comprises value added from forestry, hunting, and fishing as well as cultivation of crops and livestock production. Data are in constant 2010 U.S. dollars.",
         "source": "Derived from World Bank national accounts files and Food and Agriculture Organization, Production Yearbook and data files."
     },
-    'Food Deficit': {
-        "dataName": "FoodDeficit",
-        "fullName": "Depth of the food deficit (kilocalories per person per day)",
-        "unit_long": "(kilocalories per person per day)",
-        "unit": "kcal",
-        "description": "The depth of the food deficit indicates how many calories would be needed to lift the undernourished from their status, everything else being constant. The average intensity of food deprivation of the undernourished, estimated as the difference between the average dietary energy requirement and the average dietary energy consumption of the undernourished population (food-deprived), is multiplied by the number of undernourished to provide an estimate of the total food deficit in the country, which is then normalized by the total population.",
-        "source":  "Food and Agriculture Organization, Food Security Statistics."
+    'Gini': {
+        "dataName": 'avgGini',
+        "scaleType": "Linear",
+        'description': "",
+        "unit_long": "",
+        "unit": "",
     },
     'Fertilizer Consumption': {
         "dataName": 'FertilizerConsumpPerHA',
+        "scaleType": "Linear",
         "fullName": "Fertilizer consumption (kilograms per hectare of arable land)",
         "unit_long": "(kilograms per hectare of arable land)",
         "unit": "kg",
         "description": "Fertilizer consumption measures the quantity of plant nutrients used per unit of arable land. Fertilizer products cover nitrogenous, potash, and phosphate fertilizers (including ground rock phosphate). Traditional nutrients--animal and plant manures--are not included. For the purpose of data dissemination, FAO has adopted the concept of a calendar year (January to December). Some countries compile fertilizer data on a calendar year basis, while others are on a split-year basis. Arable land includes land defined by the FAO as land under temporary crops (double-cropped areas are counted once), temporary meadows for mowing or for pasture, land under market or kitchen gardens, and land temporarily fallow. Land abandoned as a result of shifting cultivation is excluded.",
         "source": "Food and Agriculture Organization, electronic files and web site."
-    },
-    'Gini':{
-        "dataName": 'Gini',
-        "fullName": "Income Inequality Coeficient (Gini)",
-        "unit_long": "",
-        "unit": "",
-        "description": "",
-        "source": "UNDP HDI Site"
     }
 
-    // 'Income Share of Lowest 20%':'IncomeShareLowest20'
 };
 
-var scaleMapping = {
-    // "GDP (log)": {
-    //     "dataName": "GDP",
-    //     "scaleType": "Log",
-    //     "description": "[in current US$]"
-    // },
-    // "Land Area": {
-    //     "dataName": "LandAreaSqMeters",
-    //     "scaleType": "Linear",
-    //     'description': "[in sq. meters]"
-    // },
-    "Worker Productivity": {
-        "dataName": "AgriValuePerWorker",
-        "scaleType": "Log",
-        "description": "[in US$]"
-    },
-    'Gini': {
-        "dataName": 'Gini',
-        "scaleType": "Linear",
-        'description': ""
-    }
-    // ,
-    // 'Rural Poverty': {
-    //     "dataName": 'RuralPovGap',
-    //     "scaleType": "Linear"
-    // }
-};
-
-var agData, data, metricArray;
+var agData, data;
 
 var width, height, fullSVGheight, ghostHeight;
 
@@ -72,29 +36,10 @@ var countries, crops, x;
 // set starting values
 var year = 2014,
     metric = 'Worker Productivity',
-    m, // current underlying metric
+    m = scaleMapping[metric].dataName, // current underlying metric
     curScale = 'reset'; // current scale choice
 
-//todo: add arrows to dropdown
-// create year dropdown
-var yearSelect = d3.select('.year')
-    .append('select')
-    .on('change', onYearSelect);
-
-var metricSelect = d3.select('.metric')
-    .append('select')
-    .on('change', onMetricSelect);
-
 var scaleSelect = d3.select('.scale');
-
-// map metric options to dropdown options
-var metrics = Object.keys(metricMapping);
-metricSelect.selectAll('option')
-    .data(metrics)
-    .enter()
-    .append('option')
-    .text(function(d){ return d; })
-    .property("selected", function(d){ return d === metric; }); //sets defaults value;
 
 // map scale options to buttons
 var scales = Object.keys(scaleMapping);
@@ -104,7 +49,27 @@ scaleSelect.selectAll('button')
     .append('button')
     .attr('class', function(d) { return `btn btn-outline-info scale ${d}`; })
     .html(function(d){ return d; })
-    .on('click', scale);
+    .on('click', function(d, i){
+            metric = this.innerHTML || metric;
+
+            // check to see if button was already active and save it to variable
+            let isScaled = d3.select(this).classed('active');
+
+            // remove all active tags to start fresh
+            d3.selectAll('.scale').classed('active', false);
+
+
+            if (isScaled){ // if is currently scaled, reset to full bars height
+                curScale = 'reset';
+            }
+            else { // otherwise, set the scale to value of button pushed and make button active
+                d3.select(this).classed('active', true);
+                curScale = scaleMapping[metric];
+            }
+            // set sorting metric dataName
+            m = curScale.dataName || m; // keep existing value if reset
+            updateData(); // update data with new metric
+    });
 
 // initialize arrays in order to find extrema for this data subset
 scales.forEach(function(d){ scaleMapping[d].data = []; });
@@ -152,44 +117,25 @@ function getWidthandHeight(){
 
 // get data
 d3.json('data/cropdata.json', function(err, dat){
-    agData = dat;
-
-    // get array of unique years from data -- use to populate dropdown
-    var years = _.uniq(_.map(dat, 'Time'));
-
-    // Fill in years drop down
-    yearSelect.selectAll('option')
-        .data(years)
-        .enter()
-        .append('option')
-        .text(function(d){ return d; })
-        .property("selected", function(d){ return +d === year; }); //sets defaults value
+    // reverse to get the crops in order form smallest to largest %
+    agData = _.reverse(dat);
 
     // display plot
     updateData();
 });
 
 function updateData(){
-    // initialize arrays in order to find extrema for this data subset
-    metricArray = []; // for x axis (ghost circles)
-
-    // get column name
-    m = metricMapping[metric].dataName;
-
     // filter the data to the selected year and with all the sorting metrics
     var agDataF = _.filter(agData, function(d){
-        let isFullData = true;
-
-        // checks to make sure country has data for each metric
-        metrics.forEach(function(curM, i){
-            if (!d[metricMapping[curM].dataName] || d[metricMapping[curM].dataName] === "..") isFullData = false;
-        });
-
+        let isFullData = (d[m]) ? true : false ; // check to see that data available
         return ((+d.Time === year) && isFullData);
     });
 
-    // reverse to get the crops in order form smallest to largest %
-    agDataF = _.reverse(agDataF);
+    // fill data arrays for each scale to get extrema values later
+    scales.forEach(function(i){
+        let dataName = scaleMapping[i].dataName;
+        scaleMapping[i].data = agDataF.map(function(d){ return (d[dataName])? +d[dataName] : null;});
+    });
 
     // group by country
     data = _.groupBy(agDataF, function(d){ return d.Country; });
@@ -198,21 +144,11 @@ function updateData(){
     data = _.sortBy(data, function(d){ return + d[0][m]; });
     console.log(data);
 
-    data.forEach(function(d){
-        scales.forEach(function(i){
-            let dataName = scaleMapping[i].dataName;
-            scaleMapping[i].data.push(+d[0][dataName]);
-        });
-        metricArray.push(+d[0][m]);
-    });
-
     displayBars();
     drawGhostCircles();
 }
 
 function displayBars() {
-    /*Width of Bars*/
-
     // for the cases where displayBars is called from a metric change
     d3.select('.drilldown').remove();
 
@@ -223,7 +159,7 @@ function displayBars() {
         .domain(data.map(function(d) { return d[0].Country; }));
 
     /* Begin Plotting Bars*/
-    var countryData = plot.selectAll('div.country')
+    let countryData = plot.selectAll('div.country')
         .data(data, function (d) { return d[0].Country; });
 
     countries = countryData.enter()
@@ -242,7 +178,7 @@ function displayBars() {
 
     countryData.exit().remove();
 
-    var cropData = countries.selectAll('div')
+    let cropData = countries.selectAll('div')
         .data(function (d) { return d;});
 
     crops = cropData.enter()
@@ -257,22 +193,21 @@ function displayBars() {
         .on('mouseout', function (d) { onMouseOut(d);});
 
     cropData.exit().remove();
-    scale(false);
+    scale();
     /* Finish Plotting Bars*/
 }
 
 function drawGhostCircles(){
     /* Begin Plotting Ghost Axis*/
-    var r = 7;
-    var barPadding = 10; // to make it line up with the bars below
+    let r = 7;
+    let barPadding = 5; // to make it line up with the bars below
     let tallTickHeight = 30;
 
+    console.log('metric:', metric);
     // x scale for ghost circles
     let ghostX = d3.scaleLinear()
-        .domain([d3.min(metricArray), d3.max(metricArray)])
+        .domain([d3.min(scaleMapping[metric].data), d3.max(scaleMapping[metric].data)])
         .range([r + barPadding, width - r - barPadding]);
-
-    let axis = d3.axisBottom(ghostX);
 
     var ghostCirclesData = ghostAxis.selectAll('circle')
         .data(data);
@@ -303,8 +238,6 @@ function drawGhostCircles(){
         .attr('class', 'ghostAxis')
         .attr("transform", `translate(0, ${3*r})`);
 
-    // g.call(axis);
-
     // min tick mark
     let min = g.append('g')
         .attr('class', 'tick')
@@ -321,8 +254,8 @@ function drawGhostCircles(){
         .text(function(){
             // determine whether the metric comes before or after the value
             return (metric === 'Worker Productivity') ?
-               metricMapping[metric].unit + Math.round(d3.min(metricArray)) :
-                Math.round(d3.min(metricArray)) + ' ' + metricMapping[metric].unit ;
+               scaleMapping[metric].unit + Math.round(d3.min(scaleMapping[metric].data)) :
+                Math.round(d3.min(scaleMapping[metric].data)) + ' ' + scaleMapping[metric].unit ;
         });
 
     // max tick mark
@@ -342,8 +275,8 @@ function drawGhostCircles(){
         .text(function(){
             // determine whether the metric comes before or after the value
             return (metric === 'Worker Productivity') ?
-                metricMapping[metric].unit + Math.round(d3.max(metricArray)) :
-                Math.round(d3.max(metricArray)) + ' ' + metricMapping[metric].unit;
+                scaleMapping[metric].unit + Math.round(d3.max(scaleMapping[metric].data)) :
+                Math.round(d3.max(scaleMapping[metric].data)) + ' ' + scaleMapping[metric].unit;
         });
 
     // mame of metric
@@ -353,28 +286,11 @@ function drawGhostCircles(){
         .attr('text-anchor', 'middle')
         .attr('x', width/2)
         .attr('y', tallTickHeight)
-        .text(`${metric} ${metricMapping[metric].unit_long}`)
-        // todo: change to make hover work
-        .attr('data-toggle', 'tooltip')
-        .attr('data-html', 'true')
-        .attr('data-placement', "top")
-        .attr('title',metricMapping[metric].description);
-
+        .text(`${metric} ${scaleMapping[metric].unit_long}`)
     /* Finish Plotting Ghost Axis*/
 }
 
-function scale(isReScaled = true){
-    if (isReScaled) { // case when a scale button is pushed
-        // if there is no button push, or it is the same as the previous push, reset the bars
-        curScale = ((curScale === scaleMapping[this.innerHTML]) || (scaleMapping[this.innerHTML] === undefined))
-            ? 'reset' : scaleMapping[this.innerHTML];
-
-        //todo: bug with active when clicking on multiple buttons after each other
-        // add active class when button is active
-        // d3.select(this).classed("active", d3.select(this).classed("active") ? false : true);
-    }
-    // otherwise, keep the last scale value (because it is being called from the end of displayBars()
-
+function scale(){
     if (curScale === 'reset') { // resetting bars to full height
         d3.selectAll('.countryLabel').remove();
 
@@ -383,14 +299,14 @@ function scale(isReScaled = true){
             .style('top', 0);
 
         d3.selectAll('.crop')
-            .style('height', function(d){ return ((height) * d.percentOfSubtotal); });
+            .style('height', function(d){ return ((height) * d.percentOfTotal); });
     }
     else{ // scaling the bars by the button choice
         let scaleType = 'scale'+curScale.scaleType;
         let curData = curScale.dataName; // column name corresponding to the scale choice
-        let labelDist = 5;
+        // let labelDist = 5;
 
-        var heightScale = d3[scaleType]()
+        let heightScale = d3[scaleType]()
             .domain([d3.min(curScale.data), d3.max(curScale.data)])
             .range([10, height]);
 
@@ -400,9 +316,7 @@ function scale(isReScaled = true){
             .style('top', function (d){ return height - heightScale(d[0][curData]); });
 
         d3.selectAll('.crop')
-            .style('height', function(d){ return (heightScale(d[curData]) * d.percentOfSubtotal);});
-
-        // setTimeout(callCountryLabels();
+            .style('height', function(d){ return (heightScale(d[curData]) * d.percentOfTotal);});
 
         // setTimeout(function(){
             callCountryLabels();
@@ -501,15 +415,17 @@ function onMouseover(d) {
 
     tool_tip.html(function () {
         let formattedMetric = (metric === 'Worker Productivity') ?
-            metricMapping[metric].unit + Math.round(d[m]) :
-            Math.round(d[m]) + ' ' + metricMapping[metric].unit;
+            scaleMapping[metric].unit + Math.round(d[m]) :
+            Math.round(d[m]) + ' ' + scaleMapping[metric].unit;
 
         return `<h4>${d.Country}</h4>
             <div><b>${d.Item} (${Math.round(d.percentOfTotal * 100)}% of total crops)</b></div>
             <br>
-            <p>${metric}: ${formattedMetric}</p>
+            <p>Worker Productivity: $${d.AgriValuePerWorker}</p>
             <br>
-            <p>Gini: ${d.Gini}</p>`;
+            <p>Gini: ${d.Gini}</p>
+            <br>
+            <p>Fertilizer Consumption: ${d.FertilizerConsumpPerHA}</p>`;
     })
         .style("left", (d3.event.pageX) + "px")
         .style("top", (d3.event.pageY - 28) + "px");
@@ -537,18 +453,6 @@ function countryHoverOff(country){
         .classed('hover', false);
 }
 
-function onYearSelect(){
-    console.log(this);
-    year = +d3.select(this).property('value');
-    console.log(`year changed to ${year}`);
-    updateData();
-}
-
-function onMetricSelect(){
-    metric = d3.select(this).property('value');
-    console.log(metric);
-    updateData();
-}
 
 d3.selection.prototype.moveToFront = function() {
     return this.each(function(){
@@ -561,7 +465,3 @@ window.addEventListener("resize", function(){
     displayBars();
     drawGhostCircles();
 });
-
-// $(function () {
-//     $('[data-toggle="tooltip"]').tooltip()
-// })
